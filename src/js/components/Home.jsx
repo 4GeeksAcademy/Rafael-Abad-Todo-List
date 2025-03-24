d07cb22b0035f847f9ed39b4526f91c997009c29
Rafael468 <abadginerrafa@gmail.com>
2025-03-24 18:13:11 +0000

    Initial commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,42 @@
 import React from "react";
+import { useEffect,useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [taskArray, setTaskArray]=useState([]);
+	const [input, setInput]=useState("");
+	
+	const handleKeyPress=(e)=>{
+		if (e.key === "Enter"){
+			setTaskArray([...taskArray, input]);
+			setInput("");
+		}
+	};
+	const deleteTask=(index)=>{
+			setTaskArray(taskArray.filter((uselessVariable,i)=>i!==index));
+	};
+
+
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+            <h1>Todo List</h1>
+			<input 
+			type="text"
+			placeholder="Write your task here"
+			value={input}
+			onChange={(e)=>setInput(e.target.value)}
+			onKeyDown={handleKeyPress}
+			></input>
+			<ul>
+				{taskArray.map((task,index)=>(
+					<li key={index}>{task}
+					<button onClick={()=> deleteTask(index)}>X</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
